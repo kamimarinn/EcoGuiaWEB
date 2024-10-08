b538ad5ff6481083220201ed52ddf90d6648ae5f
kamila cavalcante <kacavalcante@hotmail.com>
2024-10-08 11:49:55 -0300

    fix/adjustedstuff

---
+++ src/pages/telaEsqueciSenha/telaEsqueciSenha.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import './telaEsqueciSenha.css';
+import { Link } from "react-router-dom";
+
+function telaEsqueciSenha(){
+    return(
+        <div className='pai-container'>
+            <div className='container-senha'>
+            <img src='/img/img-telalogin.svg' alt='img-um' className='imgum-telalogin' />
+
+            </div>
+
+
+        </div>
+
+    )
+}
+
+export default telaEsqueciSenha;
