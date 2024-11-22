1ba638dc2645c01b0c636a3d69753c27e37c8ee5
kamila cavalcante <kacavalcante@hotmail.com>
2024-11-22 08:20:52 -0300

    fix/teste

--- src/routes.jsx
+++ src/routes.jsx
@@ -9,14 +9,14 @@ import Crud from "./pages/Tela CRUD/crud.jsx";
 import AddArtigos from "./pages/telaAddArtigos/AddArtigos.jsx";
 import TelaAltIcons from "./pages/telaAltIcons/telaAltIcons.jsx";
 import PagEditIcon from "./pages/PagEditIcon/PagEditIcon.jsx";
-import TelaEsqueciSenha from "./pages/TelaEsqueciSenha/TelaEsqueciSenha.jsx"
+// import TelaEsqueciSenha from "./pages/TelaEsqueciSenha/TelaEsqueciSenha.jsx";
 import TelaTrilha from "./pages/telaTrilha/telaTrilha.jsx";
 import TelaToken from "./pages/telaToken/TelaToken.jsx";
 import TelaRedefinirSenha from "./pages/telaRedefinirSenha/TelaRedefinirSenha.jsx";
 import TelaMateriais from "./pages/TelaMateriais/TelaMateriais.jsx";
 import TelaModificarLevel from "./pages/TelaModificarLevel/TelaModificarLevel.jsx";
 import TelaDicas from "./pages/TelaDicas/TelaDicas.jsx";
-import TelaDicasAdd from "./pages/TelaDicasAdd/TelaDicasAdd.jsx"
+import TelaDicasAdd from "./pages/TelaDicasAdd/TelaDicasAdd.jsx";
 
 
 export default function Rotas() {
@@ -32,7 +32,7 @@ export default function Rotas() {
                 <Route path="/AddArtigos" element={<AddArtigos/>}/>
                 <Route path="/telaAltIcons" element={<TelaAltIcons/>}/>
                 <Route path="/PagEditIcon" element={<PagEditIcon/>}/>  
-                <Route path="/TelaEsqueciSenha" element={<TelaEsqueciSenha/>}/>
+                {/* <Route path="/TelaEsqueciSenha" element={<TelaEsqueciSenha/>}/> */}
                 <Route path="/telaTrilha" element={<TelaTrilha/>}/>  
                 <Route path="/TelaToken" element={<TelaToken/>}/>    
                 <Route path="/TelaRedefinirSenha" element={<TelaRedefinirSenha/>}/> 
