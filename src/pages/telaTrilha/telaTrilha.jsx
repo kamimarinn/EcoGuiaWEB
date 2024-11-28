5b4c429818642e548ed5bc2c5f3d0ccf8bdb67b6
Kamila Cavalcante <141283305+kamimarinn@users.noreply.github.com>
2024-11-28 10:06:58 -0300

    Update telaTrilha.jsx

--- src/pages/telaTrilha/telaTrilha.jsx
+++ src/pages/telaTrilha/telaTrilha.jsx
@@ -25,21 +25,24 @@ function TelaTrilha() {
             </div>
             <div className='container-principal'>
                 <div className='container-um-quests'>
+                <div className='quest-tit'>
+                   <h3>Quests</h3>
+                   </div>
                     <div className='inputs-quests-um'>
                         <input type="number" id="input-id" className="input-quest-trilha" placeholder="Nº" />
-                        <textarea className='text-input-um' placeholder="quest" />
+                        <textarea className='text-input-um' placeholder="Quest" />
                         <input type="number" id="input-id" className="input-quest" placeholder="" />
                         </div>
 
                         <div className='inputs-quests-dois'>
                         <input type="number" id="input-id" className="input-quest-trilha" placeholder="Nº" />
-                        <textarea className='text-input-dois' placeholder="quest"/>
+                        <textarea className='text-input-dois' placeholder="Quest"/>
                         <input type="number" id="input-id" className="input-quest" placeholder="" />
                         </div>
                         
                         <div className='inputs-quests-tres'>
                         <input type="number" id="input-id" className="input-quest-trilha" placeholder="Nº" />
-                        <textarea className='text-input-tres' placeholder="quest"/>
+                        <textarea className='text-input-tres' placeholder="Quest"/>
                         <input type="number" id="input-id" className="input-quest" placeholder="" />
                         </div>
 
@@ -47,8 +50,23 @@ function TelaTrilha() {
                         
                     </div>
                     <div className='container-dois-badge'>
-                    {/* Adicionar conteúdo se necessário */}
+                   <div className='badge-tit'>
+                   <h3>Badge</h3>
+                   </div>
+                  <div className='container-inputs-badge'>
+                  <input type="file" accept="image/*" className="input-imagem" />
+                    
+                    
+                    <div className="right-column">
+                        <input type="text" placeholder="Digite o título" className="input-texto" />
+                        <textarea placeholder="Digite a descrição" className="input-descricao"></textarea>
+                  </div>
+                   
+                    </div>
                 </div>
+
+
+                
                 </div>
                
             </div>
